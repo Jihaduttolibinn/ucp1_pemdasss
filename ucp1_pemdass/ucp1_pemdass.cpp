a57ace54a325b20aa1567e9412d6d5a8a781b41a
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-29 15:38:25 +0800

    Add project files.

---
+++ ucp1_pemdass/ucp1_pemdass.cpp
@@ -0,0 +1,13 @@
+//1.contoh deklarasi intejer
+// Variabel Integer:umur 
+// variable Variabel String: nama
+// Variabel Float 
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
