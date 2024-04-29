025477bd2dba3db241b006c4acf2e3f18c266225
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-29 15:52:05 +0800

    memasukkan class Mahasiswa {

--- ucp1_pemdass/ucp1_pemdass.cpp
+++ ucp1_pemdass/ucp1_pemdass.cpp
@@ -1,11 +1,30 @@
 //1.contoh deklarasi intejer
 // Variabel Integer:umur 
 // variable Variabel String: nama
-// Variabel Float 
+// Variabel Float : tinggi_badan
+
+//2.Prosedur (Procedure):
+//def greet_user(name):
+
+//3, p = Point(10, 20) membuat objek point
+
+
+//4. prosedur 
+// def greet_user(name):
+// fungsi
+// def calculate_square(number):
+
+//5. contoh looping untuk mencetak angka dari 1 hingga 5
+// for i in range(1, 6):
+// print(i)
 
 
 #include <iostream>
 using namespace std;
+class Mahasiswa {
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
